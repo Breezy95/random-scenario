ddf7f1134c6a91420643473a826cc3a3fd95e420
EthanCampana <34409462+EthanCampana@users.noreply.github.com>
2018-01-10 11:22:26 -0500

    Bot

--- game/ScenarioBot.py
+++ game/ScenarioBot.py
@@ -3,16 +3,16 @@ import asyncio
 import random
 import time
 from discord.ext import commands
+from assets import DPlayer
 from assets import Enemy
-
 
 
 #Global Game Variables
 #---------------------------------
 global EnemyList
 EnemyList = []
-global Player
-Player = []
+global PlayerList
+PlayerList = []
 #The name of the txt file that you want to be loaded
 global ScenarioName
 ScenarioName = ""
@@ -25,38 +25,53 @@ Timer = 60
 #Minimum amount of players needed to start the scenario. Can be Adjuseted in the text file Via MinPlayers <Amount of players> on a line
 global MinPlayers
 MinPlayers = 1
-#-----------------------------------
 
+#-----------------------------------
+newplayer = DPlayer.DPlayer("Jobo",100,100,100,"",False)
+PlayerList.append(newplayer)
                        
 def Clear(): 
        global EnemyList
-       global Player
-       Player.clear()
+       global PlayerList
+       PlayerList.clear()
        EnemyList.clear()
-       return Player, EnemyList
+       return PlayerList, EnemyList
 
 
 
-def load_Enemy():
+def load():
         global ScenarioName
         with open(ScenarioName) as file:
-                for line in file:                       
+                for line in file:
+                        global PlayerList      
                         global EnemyList
                         global Timer
                         global MinPlayers
-                        arg = line.split(' ')
+                        arg = line.split(",")
+                        if 'Player' in arg:
+                               newplayer = DPlayer.DPlayer(arg[1],arg[2],arg[3],arg[4]," ",False)
+                               PlayerList.append(newplayer)
+                               arg.clear()
+                               continue
                         if 'Timer' in arg:
                                Timer = arg[1]
+                               arg.clear()
+                               continue
                         if 'MinPlayers' in arg:
                                MinPlayers = arg[1]
+                               arg.clear()
+                               continue
                         if 'Enemy' in arg:
                              global EnemyList
                              enemy1 = Enemy.Enemy(arg[1], arg[2], arg[3], arg[4])
                              EnemyList.append(enemy1)
+                             arg.clear()
+                             continue
                         if '****' in arg:
                                 return EnemyList
                                 return Timer
-                                return MinPlayers   
+                                return PlayerList
+                                return MinPlayers
                         arg.clear()
 
 #Creates the bot
@@ -73,8 +88,9 @@ async def on_ready():
 
 @bot.command(pass_context = True)
 async def test(ctx):
-       name = await bot.wait_for_message()
-       await bot.say(name.content)
+       arg = ctx.message.content
+       await bot.say(arg)
+       
 
        
 @bot.command(pass_context = True)
@@ -103,6 +119,15 @@ async def mInfo(ctx, arg: int):
 #Create Functions and Commands Below vvv
 
 @bot.command(pass_context = True)
+async def playerlist(ctx):
+       global PlayerList
+       for players in PlayerList:
+              await bot.say(players.name)
+              await bot.say(str(players.hasID))
+       
+
+
+@bot.command(pass_context = True)
 async def loadEnemy(ctx):
         try:
                 Clear()
@@ -128,7 +153,7 @@ async def ts(ctx, filename):
                         ScenarioName = filename    
                         try:
                                Clear()
-                               load_Enemy()
+                               load()
                         except:
                                await bot.say(ctx.author.mention + " Scenario was found but failed to load. Check the scenario file to make sure everything is up to standard.")
                         await bot.delete_message(ctx.message)
@@ -138,7 +163,7 @@ async def ts(ctx, filename):
                 name2 = filename.split(".")
                 try:
                                Clear()
-                               load_Enemy()
+                               load()
                 except:
                                await bot.say(ctx.author.mention + " Scenario was found but failed to load. Check the scenario file to make sure everything is up to standard.")
                 await bot.say(name2[0] + " scenario has been loaded and ready to play")
@@ -163,15 +188,34 @@ async def gameSettings(ctx):
 
 
 
-
+@bot.command(pass_context = True)
+async def join(ctx):
+       global PlayerList
+       playernumber = ctx.message.content.split(" ")
+       if len(playernumber) > 1:
+              newplayernumber = int(playnumber[1])
+              index = newplayernumber - 1
+              PlayerList[index].ID = ctx.message.author.ID
+              PlayerList[Index].hasID = True
+              await bot.say(ctx.author.mention + " You Joined in as Player " + str(newplayernumber))
+              return PlayerList
+       for player in PlayerList:  
+              if player.hasID == False:
+                     player.ID == ctx.message.author.id
+                     player.hasID == True
+                     await bot.say(ctx.message.author.mention + " You Joined in as Player" + player.name)
+                     return player
+       return PlayerList        
+              
 @bot.command(pass_context = True)
 async def Play(ctx): 
        global isPlaying
        global MinPlayers
+       playercount = int(len(Player))
        if ScenarioName == "":
               await bot.say("There is no Scenario Selected or Loaded. Select a Scenario with the !ts <ScenarioName> command")
-              return
-       if len(Player) < MinPlayers:
+              return 
+       if playercount < 10:
               await bot.say("Not Enough Players")
               return
        isPlaying == True
